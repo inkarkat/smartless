f5b6da3501c0a74d5930b06e0c4dba5a7c4a538f
Ingo Karkat <swdev@ingo-karkat.de>
2024-10-01 16:56:32 +0200

    exports: Robustness: Use preconfigured $PAGER, only set SMARTLESS_PAGER_ARGUMENTS if less
    Some barebones systems (Synology NAS after a system upgrade with the Optware packages off the PATH) just have "more".

--- profile/exports.sh
+++ profile/exports.sh
@@ -5,9 +5,13 @@
 # resorting to --no-init; i.e. still shows small output that fits the terminal,
 # yet does not show browsed text fragments because it uses the alternate screen.
 # (http://unix.stackexchange.com/questions/107315/less-quit-if-one-screen-without-no-init/107355)
-export PAGER=smartless
-export SMARTLESS_PAGER='less' # the pager to be used
-export SMARTLESS_PAGER_ARGUMENTS='--RAW-CONTROL-CHARS' # the default arguments to the pager
+if [ "$PAGER" != smartless ]; then
+    export SMARTLESS_PAGER="${PAGER:-less}" # the actual pager to be used
+    if [ "$SMARTLESS_PAGER" = less ]; then
+	export SMARTLESS_PAGER_ARGUMENTS='--RAW-CONTROL-CHARS' # the default arguments to the pager
+    fi
+    export PAGER=smartless
+fi
 export SMARTLESS_COLOR='[3;36m'
 export SMARTLESS_CAPTURE_DIR="${XDG_RUNTIME_DIR:-${TMPDIR:-/tmp}/${UID:-$USER}}/smartless"
 [ -d "$SMARTLESS_CAPTURE_DIR" ] || \
